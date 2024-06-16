30d4dd1415dc03b604f5ccf7dc18df014a23b8eb
matien19 <matienkasa@gmail.com>
2024-06-17 00:36:48 +0700

    add pesanan saya in transaksi

--- src/controllers/TransaksiController.js
+++ src/controllers/TransaksiController.js
@@ -17,11 +17,19 @@ export const getTransaksiByNo = async (req, res) => {
     console.log(error.message);
   }
 };
+export const getTransaksiSaya = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const [response] = await db.query(`SELECT * FROM tbl_transaksi WHERE id_user = ?`, [userId]);
+    res.json(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const createTransaksi = async (req, res) => {
-  const {noTransaksi, idUser, idArmada, idDriver, tglMulai, durasi, price, alamat } = req.body;
+  const { noTransaksi, idUser, idArmada, idDriver, tglMulai, durasi, price, alamat } = req.body;
   const total = price * durasi;
-  
 
   try {
     await db.query(`INSERT INTO tbl_transaksi (no_transaksi, id_user, id_armada, id_driver, tgl_mulai, durasi, total, alamat) values (?, ?, ?, ?, ?, ?, ?, ?)`, [noTransaksi, idUser, idArmada, idDriver, tglMulai, durasi, total, alamat]);
--- src/routes/TransaksiRouter.js
+++ src/routes/TransaksiRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createTransaksi, getTransaksi, getTransaksiByNo } from "../controllers/TransaksiController.js";
+import { getTransaksiSaya, createTransaksi, getTransaksi, getTransaksiByNo } from "../controllers/TransaksiController.js";
 
 const router = express.Router();
 
 router.get("/transaksi", getTransaksi);
 router.get("/transaksi/:no_transaksi", getTransaksiByNo);
 router.post("/transaksi", createTransaksi);
+router.get("/transaksi_saya/:id", getTransaksiSaya);
 
 export default router;
